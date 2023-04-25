751ba957a9b56db08a74cf3fa4aa97ade55c1abd
dongjin6539 <dongjin6539@gmail.com>
2023-04-25 20:24:58 +0900

    2023.04.25

---
+++ javascript/game/js/music.js
@@ -0,0 +1,53 @@
+const allMusic = [
+    {
+        name : "1. Boom Bap Flick",
+        artist : "Quincas Moreira",
+        img : "music_view01",
+        audio : "music_audio01"
+    },{
+        name : "2. Can't Hide",
+        artist : "Otis McDonald",
+        img : "music_view01",
+        audio : "music_audio02"
+    },{
+        name : "3. Emotional Mess",
+        artist : "Amy Lynn & the Honey Men",
+        img : "music_view01",
+        audio : "music_audio03"
+    },{
+        name : "4. Forever Young",
+        artist : "Otis McDonald",
+        img : "music_view01",
+        audio : "music_audio04"
+    },{
+        name : "5. I Wish I Knew",
+        artist : "Otis McDonald",
+        img : "music_view01",
+        audio : "music_audio05"
+    },{
+        name : "6. Ice & Fire",
+        artist : "King Canyon",
+        img : "music_view01",
+        audio : "music_audio06"
+    },{
+        name : "7. Jingle Bells",
+        artist : "DJ Williams",
+        img : "music_view01",
+        audio : "music_audio07"
+    },{
+        name : "8. Mulholland",
+        artist : "King Canyon",
+        img : "music_view01",
+        audio : "music_audio08"
+    },{
+        name : "9. Spooky Boop",
+        artist : "Otis McDonald",
+        img : "music_view01",
+        audio : "music_audio09"
+    },{
+        name : "10. We Cruisin'",
+        artist : "Otis McDonald",
+        img : "music_view01",
+        audio : "music_audio10"
+    }
+]
