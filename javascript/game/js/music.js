3259a8189e8bce57d57935cdb758923f5e8763da
dongjin6539 <dongjin6539@gmail.com>
2023-04-26 17:55:32 +0900

    2023.04.26

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -1,53 +1,76 @@
 const allMusic = [
     {
-        name : "1. Boom Bap Flick",
+        name : "Boom Bap Flick",
         artist : "Quincas Moreira",
         img : "music_view01",
         audio : "music_audio01"
     },{
-        name : "2. Can't Hide",
-        artist : "Otis McDonald",
-        img : "music_view01",
+        name : "It Was a Time",
+        artist : "TrackTribe",
+        img : "music_view02",
         audio : "music_audio02"
     },{
-        name : "3. Emotional Mess",
+        name : "Emotional Mess",
         artist : "Amy Lynn & the Honey Men",
-        img : "music_view01",
+        img : "music_view03",
         audio : "music_audio03"
     },{
-        name : "4. Forever Young",
+        name : "Forever Young",
         artist : "Otis McDonald",
-        img : "music_view01",
+        img : "music_view04",
         audio : "music_audio04"
     },{
-        name : "5. I Wish I Knew",
+        name : "I Wish I Knew",
         artist : "Otis McDonald",
-        img : "music_view01",
+        img : "music_view05",
         audio : "music_audio05"
     },{
-        name : "6. Ice & Fire",
+        name : "Ice & Fire",
         artist : "King Canyon",
-        img : "music_view01",
+        img : "music_view06",
         audio : "music_audio06"
     },{
-        name : "7. Jingle Bells",
+        name : "Jingle Bells",
         artist : "DJ Williams",
-        img : "music_view01",
+        img : "music_view07",
         audio : "music_audio07"
     },{
-        name : "8. Mulholland",
+        name : "Mulholland",
         artist : "King Canyon",
-        img : "music_view01",
+        img : "music_view08",
         audio : "music_audio08"
     },{
-        name : "9. Spooky Boop",
+        name : "Spooky Boop",
         artist : "Otis McDonald",
-        img : "music_view01",
+        img : "music_view09",
         audio : "music_audio09"
     },{
-        name : "10. We Cruisin'",
+        name : "We Cruisin'",
         artist : "Otis McDonald",
-        img : "music_view01",
+        img : "music_view10",
         audio : "music_audio10"
     }
-]
+];
+
+const musicWrap = document.querySelector(".music__wrap");
+const musicName = musicWrap.querySelector(".music__control .title h3");
+const musicArtist = musicWrap.querySelector(".music__control .title p");
+const musicView = musicWrap.querySelector(".music__view .image img");
+const musicAudio = musicWrap.querySelector("#main-audio");
+
+let musicIndex = 2; // 현재 음악 인덱스
+
+// 음악 재생
+const loadMusic = (num) => {
+    musicName.innerText = allMusic[num-1].name;             // 뮤직 제목
+    musicArtist.innerText = allMusic[num-1].artist;         // 뮤직 아티스트
+    musicView.src = `img/${allMusic[num-1].img}.png`;       // 뮤직 이미지
+    musicView.alt = allMusic[num-1].name;                   // 뮤직 이미지 alt
+    musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`;  // 뮤직 오디오 파일
+};
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+
+    musicAudio.play();
+});
