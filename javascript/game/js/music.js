648c9ec0746530845becc5ef0b09386462246c65
dongjin6539 <dongjin6539@gmail.com>
2023-04-27 17:52:42 +0900

    2023.04.27

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -1,51 +1,51 @@
 const allMusic = [
     {
-        name : "Boom Bap Flick",
+        name : "1. Boom Bap Flick",
         artist : "Quincas Moreira",
         img : "music_view01",
         audio : "music_audio01"
     },{
-        name : "It Was a Time",
+        name : "2. It Was a Time",
         artist : "TrackTribe",
         img : "music_view02",
         audio : "music_audio02"
     },{
-        name : "Emotional Mess",
+        name : "3. Emotional Mess",
         artist : "Amy Lynn & the Honey Men",
         img : "music_view03",
         audio : "music_audio03"
     },{
-        name : "Forever Young",
+        name : "4. Forever Young",
         artist : "Otis McDonald",
         img : "music_view04",
         audio : "music_audio04"
     },{
-        name : "I Wish I Knew",
+        name : "5. I Wish I Knew",
         artist : "Otis McDonald",
         img : "music_view05",
         audio : "music_audio05"
     },{
-        name : "Ice & Fire",
+        name : "6. Ice & Fire",
         artist : "King Canyon",
         img : "music_view06",
         audio : "music_audio06"
     },{
-        name : "Jingle Bells",
+        name : "7. Jingle Bells",
         artist : "DJ Williams",
         img : "music_view07",
         audio : "music_audio07"
     },{
-        name : "Mulholland",
+        name : "8. Mulholland",
         artist : "King Canyon",
         img : "music_view08",
         audio : "music_audio08"
     },{
-        name : "Spooky Boop",
+        name : "9. Spooky Boop",
         artist : "Otis McDonald",
         img : "music_view09",
         audio : "music_audio09"
     },{
-        name : "We Cruisin'",
+        name : "10. We Cruisin'",
         artist : "Otis McDonald",
         img : "music_view10",
         audio : "music_audio10"
@@ -58,8 +58,14 @@ const musicArtist = musicWrap.querySelector(".music__control .title p");
 const musicView = musicWrap.querySelector(".music__view .image img");
 const musicAudio = musicWrap.querySelector("#main-audio");
 const musicPlay = musicWrap.querySelector("#control-play");
+const musicPrevBtn = musicWrap.querySelector("#control-prev");
+const musicNextBtn = musicWrap.querySelector("#control-next");
+const musicProgress = musicWrap.querySelector(".progress");
+const musicProgressBar = musicWrap.querySelector(".progress .bar");
+const musicProgressCurrent = musicWrap.querySelector(".progress .timer .current");
+const musicProgressDuration = musicWrap.querySelector(".progress .timer .duration");
 
-let musicIndex = 2; // 현재 음악 인덱스
+let musicIndex = 3; // 현재 음악 인덱스
 
 // 음악 재생
 const loadMusic = (num) => {
@@ -70,10 +76,98 @@ const loadMusic = (num) => {
     musicAudio.src = `audio/${allMusic[num-1].audio}.mp3`;  // 뮤직 오디오 파일
 };
 
+// 재생
+const playMusic = () => {
+    musicWrap.classList.add("paused");
+    musicPlay.setAttribute("title", "정지");
+    musicPlay.setAttribute("class", "stop");
+    musicAudio.play();
+};
+
+// 정지
+const pauseMusic = () => {
+    musicWrap.classList.remove("paused");
+    musicPlay.setAttribute("title", "재생");
+    musicPlay.setAttribute("class", "play");
+    musicAudio.pause();
+};
+
+// 이전 곡 듣기
+const prevMusic = () => {
+
+    // 무한반복 if문
+    // musicIndex--;
+    // if(musicIndex == 0)musicIndex = allMusic.length;
+
+    // 무한반복 삼항연산자
+    musicIndex == 1 ? musicIndex = allMusic.length : musicIndex--;
+
+    loadMusic(musicIndex);
+    playMusic();
+};
+
+// 다음 곡 듣기
+const nextMusic = () => {
+
+    // 무한반복 if문
+    // musicIndex++;
+    // if(musicIndex == allMusic.length+1)musicIndex = 1;
+
+    // 무한반복 삼항연산자
+    musicIndex == allMusic.length ? musicIndex = 1 : musicIndex++;
+
+    loadMusic(musicIndex);
+    playMusic();
+};
+
+// 뮤직 진행바
+musicAudio.addEventListener("timeupdate", e => {
+    console.log(e);
+    const currentTime = e.target.currentTime;           // 현재 재생되는 시간
+    const duration = e.target.duration;                 // 오디오의 총 길이
+    let progressWidth = (currentTime/duration) * 100;   // 전체길이에서 현재 진행되는 시간을 백분위 단위로 나누면 현재 시간을 알 수 있음
+
+    musicProgressBar.style.width = `${progressWidth}%`;
+
+    // 전체 시간
+    musicAudio.addEventListener("loadeddata", () => {
+        let audioDuration = musicAudio.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if(totalSec < 10) totalSec = `0${totalSec}`
+        musicProgressDuration.innerText = `${totalMin}:${totalSec}`;
+    });
+
+    // 진행 시간
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if(currentSec < 10) currentSec = `0${currentSec}`
+    musicProgressCurrent.innerText = `${currentMin}:${currentSec}`;
+});
+
+// 진행 바 클릭
+musicProgress.addEventListener("click", (e) => {
+    let progressWidth = musicProgress.clientWidth;  // 진행바 전체 길이
+    let clickddOffsetX = e.offsetX;                   // 진행바를 기준으로 측정되는 X좌표 값
+    let songDuration = musicAudio.duration;         // 오디오 전체 길이
+
+    // 백분위로 나눈 숫자에 다시 전체 길이를 곱해서 현재 재생값으로 바꿈
+    musicAudio.currentTime = (clickddOffsetX/progressWidth) * songDuration;
+});
 // 플레이 버튼 클릭
 musicPlay.addEventListener("click", () => {
-    musicAudio.play();
-    
+    const isMUsicPaused = musicWrap.classList.contains("paused");   // 음악 재생 중
+    isMUsicPaused ? pauseMusic() : playMusic();
+});
+
+// 이전곡 버튼 클릭
+musicPrevBtn.addEventListener("click", () => {
+    prevMusic();
+});
+
+// 다음곡 버튼 클릭
+musicNextBtn.addEventListener("click", () => {
+    nextMusic();
 });
 
 window.addEventListener("load", () => {
