b5bdfda2d525768bd61e23dd6ed17fc1ca478c26
dongjin6539 <dongjin6539@gmail.com>
2023-07-25 15:23:48 +0900

    again

---
+++ javascript/game/README.md
@@ -0,0 +1,15 @@
+# Game2023
+
+![image](https://github.com/dongjin6539/car_showcase/blob/main/image.png)
+
+게임을 배경으로 뮤직 플레이어, 테트리스 게임을 구현했습니다.
+script에서 javascript, jQuery, GSAP 를 사용했습니다.
+주기적으로 현재 시간과 사용되고 있는 시스템 정보에 대해 설정했습니다.
+
+## MusicPlayer
+
+![image](https://github.com/dongjin6539/car_showcase/blob/main/image.png)
+
+## Tetris
+
+![image](https://github.com/dongjin6539/car_showcase/blob/main/image.png)
--- javascript/game/js/tetris.js
+++ javascript/game/js/tetris.js
@@ -8,7 +8,9 @@ const tetrisMethod = document.querySelector(".tetris__method");
 const tetrisTime = document.querySelector(".tetris__time span");
 const tetrisResult = document.querySelector(".tetris__result");
 const resultTime = document.querySelector(".tetris__result .tetris__time span");
-const resultScore = document.querySelector(".tetris__result .tetris__score span");
+const resultScore = document.querySelector(
+    ".tetris__result .tetris__score span"
+);
 
 // 음악
 let tetrisMusic = new Audio(
@@ -24,51 +26,190 @@ let tetrisGameOver = new Audio(
     "https://dongjin6539.github.io/web2023/javascript/game/audio/tetrisGameOver.mp3"
 );
 
-
 const line_rows = 20; //가로
 const line_cols = 12; //세로
 const blocks = {
     Tmino: [
-        [[2,1], [0,1], [1,0], [1,1]],
-        [[1,2], [0,1], [1,0], [1,1]],
-        [[1,2], [0,1], [2,1], [1,1]],
-        [[1,2], [1,0], [2,1], [1,1]],
+        [
+            [2, 1],
+            [0, 1],
+            [1, 0],
+            [1, 1],
+        ],
+        [
+            [1, 2],
+            [0, 1],
+            [1, 0],
+            [1, 1],
+        ],
+        [
+            [1, 2],
+            [0, 1],
+            [2, 1],
+            [1, 1],
+        ],
+        [
+            [1, 2],
+            [1, 0],
+            [2, 1],
+            [1, 1],
+        ],
     ],
     Imino: [
-        [[0,0], [1,0], [2,0], [3,0]],
-        [[2,0], [2,1], [2,2], [2,3]],
-        [[0,0], [1,0], [2,0], [3,0]],
-        [[2,0], [2,1], [2,2], [2,3]],
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [3, 0],
+        ],
+        [
+            [2, 0],
+            [2, 1],
+            [2, 2],
+            [2, 3],
+        ],
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [3, 0],
+        ],
+        [
+            [2, 0],
+            [2, 1],
+            [2, 2],
+            [2, 3],
+        ],
     ],
     Omino: [
-        [[0,0], [1,0], [0,1], [1,1]],
-        [[0,0], [1,0], [0,1], [1,1]],
-        [[0,0], [1,0], [0,1], [1,1]],
-        [[0,0], [1,0], [0,1], [1,1]],
+        [
+            [0, 0],
+            [1, 0],
+            [0, 1],
+            [1, 1],
+        ],
+        [
+            [0, 0],
+            [1, 0],
+            [0, 1],
+            [1, 1],
+        ],
+        [
+            [0, 0],
+            [1, 0],
+            [0, 1],
+            [1, 1],
+        ],
+        [
+            [0, 0],
+            [1, 0],
+            [0, 1],
+            [1, 1],
+        ],
     ],
     Zmino: [
-        [[0,0], [1,0], [1,1], [2,1]],
-        [[1,0], [0,1], [1,1], [0,2]],
-        [[0,0], [1,0], [1,1], [2,1]],
-        [[1,0], [0,1], [1,1], [0,2]],
+        [
+            [0, 0],
+            [1, 0],
+            [1, 1],
+            [2, 1],
+        ],
+        [
+            [1, 0],
+            [0, 1],
+            [1, 1],
+            [0, 2],
+        ],
+        [
+            [0, 0],
+            [1, 0],
+            [1, 1],
+            [2, 1],
+        ],
+        [
+            [1, 0],
+            [0, 1],
+            [1, 1],
+            [0, 2],
+        ],
     ],
     Smino: [
-        [[1,0], [2,0], [1,1], [0,1]],
-        [[0,0], [0,1], [1,1], [1,2]],
-        [[1,0], [2,0], [1,1], [0,1]],
-        [[0,0], [0,1], [1,1], [1,2]],
+        [
+            [1, 0],
+            [2, 0],
+            [1, 1],
+            [0, 1],
+        ],
+        [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 2],
+        ],
+        [
+            [1, 0],
+            [2, 0],
+            [1, 1],
+            [0, 1],
+        ],
+        [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 2],
+        ],
     ],
     Jmino: [
-        [[0,0], [1,0], [2,0], [2,1]],
-        [[1,0], [2,0], [1,1], [1,2]],
-        [[0,0], [0,1], [1,1], [2,1]],
-        [[1,0], [0,2], [1,1], [1,2]],
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [2, 1],
+        ],
+        [
+            [1, 0],
+            [2, 0],
+            [1, 1],
+            [1, 2],
+        ],
+        [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [2, 1],
+        ],
+        [
+            [1, 0],
+            [0, 2],
+            [1, 1],
+            [1, 2],
+        ],
     ],
     Lmino: [
-        [[0,0], [1,0], [2,0], [0,1]],
-        [[1,0], [2,2], [1,1], [1,2]],
-        [[2,0], [0,1], [1,1], [2,1]],
-        [[1,0], [0,0], [1,1], [1,2]],
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [0, 1],
+        ],
+        [
+            [1, 0],
+            [2, 2],
+            [1, 1],
+            [1, 2],
+        ],
+        [
+            [2, 0],
+            [0, 1],
+            [1, 1],
+            [2, 1],
+        ],
+        [
+            [1, 0],
+            [0, 0],
+            [1, 1],
+            [1, 2],
+        ],
     ],
 };
 
@@ -80,7 +221,7 @@ let tempMovingItem;
 let stopGame = false;
 //블록의 정보 변수
 const movingItem = {
-    type: '',
+    type: "",
     direction: 0,
     top: 0,
     left: 4,
@@ -88,15 +229,15 @@ const movingItem = {
 
 /**  시작하기 */
 function init() {
-    tempMovingItem = {...movingItem};
+    tempMovingItem = { ...movingItem };
 
     for (let i = 0; i < line_rows; i++) {
         newLine(); //라인만들기
     }
-    
+
     // generateNewBlock(); //다음 블럭 생성
     // renderBlocks(); //블록 만들기
-};
+}
 
 // 라인만들기
 function newLine() {
@@ -111,8 +252,8 @@ function newLine() {
 }
 
 // 블록 만들기
-function renderBlocks(moveType){
-    if(!stopGame){
+function renderBlocks(moveType) {
+    if (!stopGame) {
         // console.log(movingItem.type);
         // console.log(movingItem.direction);
         // console.log(movingItem.top);
@@ -121,27 +262,29 @@ function renderBlocks(moveType){
         // const direction = movingItem.direction;
         // const top = movingItem.top;
         // const left = movingItem.left;
-        const {type,direction,top,left} = tempMovingItem;
+        const { type, direction, top, left } = tempMovingItem;
         // console.log(type,direction,top,left)
         const movingBlocks = document.querySelectorAll(".moving");
-        movingBlocks.forEach(moving => {
-            moving.classList.remove(type, "moving")
-        })
-        blocks[type][direction].some(block => {
-            const x = block[0]+left;
-            const y = block[1]+top;
-            const target = tetrisView.childNodes[y] ? tetrisView.childNodes[y].childNodes[0].childNodes[x] : null;
+        movingBlocks.forEach((moving) => {
+            moving.classList.remove(type, "moving");
+        });
+        blocks[type][direction].some((block) => {
+            const x = block[0] + left;
+            const y = block[1] + top;
+            const target = tetrisView.childNodes[y]
+                ? tetrisView.childNodes[y].childNodes[0].childNodes[x]
+                : null;
             const isAvailable = checkEmpty(target);
-            if(isAvailable){
+            if (isAvailable) {
                 target.classList.add(type, "moving");
             } else {
-                tempMovingItem = {...movingItem};
+                tempMovingItem = { ...movingItem };
                 setTimeout(() => {
                     renderBlocks();
-                    if(moveType === "top"){
+                    if (moveType === "top") {
                         seizeBlock();
                     }
-                },1);
+                }, 1);
                 return true;
             }
         });
@@ -151,9 +294,9 @@ function renderBlocks(moveType){
     }
 }
 
-function seizeBlock(){
+function seizeBlock() {
     const movingBlocks = document.querySelectorAll(".moving");
-    movingBlocks.forEach(moving => {
+    movingBlocks.forEach((moving) => {
         moving.classList.remove("moving");
         moving.classList.add("seized");
     });
@@ -162,26 +305,26 @@ function seizeBlock(){
 }
 
 // 줄 제거하기
-function checkMatch(){
+function checkMatch() {
     const childNodes = tetrisView.childNodes;
 
     childNodes[0].children[0].childNodes.forEach((li) => {
-        if(li.classList.contains("seized")){
+        if (li.classList.contains("seized")) {
             stopGame = true;
             tetrisGameOver.play();
             tetrisGameover();
         }
     });
 
-    childNodes.forEach(child => {
+    childNodes.forEach((child) => {
         let matched = true;
-        child.children[0].childNodes.forEach(li => {
-            if(!li.classList.contains("seized")){
+        child.children[0].childNodes.forEach((li) => {
+            if (!li.classList.contains("seized")) {
                 matched = false;
             }
         });
 
-        if(matched){
+        if (matched) {
             tetrisRemove.play();
             child.remove();
             newLine();
@@ -201,58 +344,58 @@ function checkMatch(){
                     break;
             }
         }
-    })
+    });
     generateNewBlock();
 }
 
 // 다음 블럭 생성
-function generateNewBlock(){
+function generateNewBlock() {
     clearInterval(downInterval);
-    downInterval = setInterval(()=>{
-        moveBlock("top", 1)
-    }, duration)
+    downInterval = setInterval(() => {
+        moveBlock("top", 1);
+    }, duration);
     const blockArray = Object.entries(blocks);
-    const randomIndex = Math.floor(Math.random() * blockArray.length)
+    const randomIndex = Math.floor(Math.random() * blockArray.length);
     movingItem.type = blockArray[randomIndex][0];
     movingItem.top = 0;
     movingItem.left = 4;
     movingItem.direction = 0;
-    tempMovingItem = {...movingItem};
+    tempMovingItem = { ...movingItem };
     renderBlocks();
-
-    
 }
 
-function checkEmpty(target){
-    if(!target || target.classList.contains("seized")){
+function checkEmpty(target) {
+    if (!target || target.classList.contains("seized")) {
         return false;
     }
     return true;
 }
 
 /** 블록 움직이기*/
-function moveBlock (moveType, amount){
+function moveBlock(moveType, amount) {
     tempMovingItem[moveType] += amount;
     renderBlocks(moveType);
 }
 
 // 모양 변경하기
-function changeDirection (){
+function changeDirection() {
     const direction = tempMovingItem.direction;
-    direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1;
+    direction === 3
+        ? (tempMovingItem.direction = 0)
+        : (tempMovingItem.direction += 1);
     renderBlocks();
 }
 
 // 스페이스바 누르기
-function dropBlock (){
+function dropBlock() {
     clearInterval(downInterval);
     downInterval = setInterval(() => {
-        moveBlock("top", 1)
-    },10)
+        moveBlock("top", 1);
+    }, 10);
 }
 
-document.addEventListener("keydown", e => {
-    switch(e.keyCode){
+document.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
         case 39:
             moveBlock("left", 1);
             break;
@@ -274,7 +417,7 @@ document.addEventListener("keydown", e => {
 });
 
 // 시간 설정
-function setTime(){
+function setTime() {
     setGameTime = setInterval(() => {
         time++;
         tetrisTime.innerText = time;
@@ -282,7 +425,7 @@ function setTime(){
 }
 
 // 게임 오버
-function tetrisGameover(){
+function tetrisGameover() {
     tetrisMusic.pause();
     tetrisMusic.currentTime = 0;
     tetrisTitle.style.display = "none";
@@ -296,7 +439,7 @@ function tetrisGameover(){
 }
 
 // 게임 스타트
-function gameStart(){
+function gameStart() {
     stopGame = false;
     tetrisStart.style.display = "none";
     tetrisTitle.style.display = "block";
@@ -308,9 +451,9 @@ function gameStart(){
     setTime();
     tetrisMusic.play();
     tetrisMusic.loop = true;
-};
+}
 
-function gameReset(){
+function gameReset() {
     tetrisMusic.pause();
     tetrisMusic.currentTime = 0;
     clearInterval(setGameTime);
@@ -321,25 +464,31 @@ function gameReset(){
     tetrisTime.innerText = time;
     scoreDisplay.innerText = score;
 
-    const tetrisMinos = document.querySelectorAll(".tetris__play .view ul li ul li");
-        tetrisMinos.forEach((minos) => {
+    const tetrisMinos = document.querySelectorAll(
+        ".tetris__play .view ul li ul li"
+    );
+    tetrisMinos.forEach((minos) => {
         minos.className = "";
     });
-};
+}
 
-document.querySelector(".tetris__result button").addEventListener("click", () => {
-    tetrisStart.style.display = "block";
-    tetrisResult.style.display = "none";
-    gameReset();
-});
+document
+    .querySelector(".tetris__result button")
+    .addEventListener("click", () => {
+        tetrisStart.style.display = "block";
+        tetrisResult.style.display = "none";
+        gameReset();
+    });
 
-document.querySelector(".tetris__start button").addEventListener("click", () => {
-    gameStart();
-});
+document
+    .querySelector(".tetris__start button")
+    .addEventListener("click", () => {
+        gameStart();
+    });
 
 document.querySelector(".tetris__close").addEventListener("click", () => {
     document.querySelector(".tetris__wrap").style.display = "none";
-})
+});
 
 window.addEventListener("DOMContentLoaded", () => {
     init();
